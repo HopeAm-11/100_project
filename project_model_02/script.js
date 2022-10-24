2ef9ba459d52519a3f8964bc8ad72fd2daf63e44
HopeAm-11 <amalkhalifa11@gmail.com>
2022-10-24 12:00:06 +0400

    added dark mode

--- project_model_02/script.js
+++ project_model_02/script.js
@@ -1,2 +1,11 @@
 const button = document.querySelector('#btn');
 const container = document.querySelector('#box_model');
+const parent = document.querySelector('.container');
+
+
+button.addEventListener('click', function () {
+    container.style.display = "block";
+    // container.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.527)), url(images/bg.jpg) no-repeat center fixed;"
+    // background: -o-
+    parent.classList.remove("container");
+})
