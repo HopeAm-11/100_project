091ffd7a32d0b74ecc46cecf3db4e460619a779e
HopeAm-11 <amalkhalifa11@gmail.com>
2022-10-24 13:06:15 +0400

    added remove

--- project_model_02/script.js
+++ project_model_02/script.js
@@ -1,9 +1,16 @@
 const button = document.querySelector('#btn');
 const container = document.querySelector('#box_model');
 const parent = document.querySelector('.container');
+const close = document.querySelector('#remove');
 
 
 button.addEventListener('click', function () {
     container.style.display = "block";
     parent.classList.remove("container");
+})
+
+close.addEventListener('click', function () {
+    container.style.display = "none";
+    parent.classList.add("container");
+
 })
