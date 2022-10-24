9aa8ea44ef7b77a77d89267f367f8b59ec14c114
HopeAm-11 <amalkhalifa11@gmail.com>
2022-10-24 12:05:25 +0400

    added article and header

--- project_model_02/script.js
+++ project_model_02/script.js
@@ -5,7 +5,5 @@ const parent = document.querySelector('.container');
 
 button.addEventListener('click', function () {
     container.style.display = "block";
-    // container.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.527)), url(images/bg.jpg) no-repeat center fixed;"
-    // background: -o-
     parent.classList.remove("container");
 })
