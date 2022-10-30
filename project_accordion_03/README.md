3a57a1fb21a1bff3be827d6ae1ac2570f7cd41c9
HopeAm-11 <amalkhalifa11@gmail.com>
2022-10-30 11:17:10 +0400

    progress

--- project_accordion_03/README.md
+++ project_accordion_03/README.md
@@ -3,16 +3,6 @@
 - Make a Q&A page with Accordion layout.
      An accordion is used to show (and hide) HTML content.
 
-# Planing:
-
-1. user click on the arrow or the box it reveals answer below.
-     - How to make 1 event listener so that each box will reveal its own information. 
-          - each sections has its own hidden paragraph 
-          - when user click on the section it will reveal it hidden paragraph
-          
-     
-
-
 
 # Credits 
 
--- project_accordion_03/script.js
+++ project_accordion_03/script.js
@@ -3,68 +3,53 @@ const parentContainer = document.querySelector('#main'); // parent of sections
 const sections = document.querySelector('#section');
 const section = document.querySelectorAll('#section');
 
-// const icon = document.querySelectorAll('#icons');
+const icon = document.querySelector('#icons');
 
-// sections.addEventListener('click', () => {
-
-//     answerContainer.style.display = 'block'
-
-// })
-
-
-parentContainer.addEventListener('click', function (e) {
-    const target = e.target;
-    console.log(target)
-
-
-    if (target.matches('#icons')) {
-        if (answerContainer.style.display = 'none') {
-            e.target.parentNode.parentNode.lastElementChild.style.display = 'block'
-
-        } else {
-            e.target.parentNode.parentNode.lastElementChild.style.display = 'none'
-
-        }
-    }
-
-})
-
-
-
-// parentContainer.addEventListener('click', (event) => {
-//     accordion(event);
-// });
-
-
-
-// function accordion() {
-//     const btn = event.target.closest('[accordion-trigger]');
-//     const expanded = btn.getAttribute('aria-expanded') === 'true';
-//     const target = btn.parentNode.nextElementSibling;
-
-//     btn.setAttribute('aria-expanded', !expanded);
-//     target.hidden = expanded;
-// }
-
-
-
-
-
-
-
-// parentContainer.addEventListener('click', function (e) {
-//     let target = e.target; // where was the click?
-//     if (target.nodeName === 'i') {
-//         // const parent = target.closest('.hide');
-//         const panels = container.querySelectorAll('.hide');
-//         panels.forEach(panel => panel.classList.remove('hide'));
-
-
-//         // container.style.display = 'block';
-//         // main.children[0].style.display = 'block';
-//         // container.classList.remove('hide');
-
-//         console.log(target)
+// icon.addEventListener('click', () => {
+//     if (answerContainer.classList == 'hide') {
+//         answerContainer.classList.remove("hide");
+//     } else {
+//         answerContainer.classList.add("hide");
 
 //     }
-// });
+// })
+var i;
+
+for (i = 0; i < section.length; i++) {
+    section[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        // console.log(this) the section that been clicked 
+        // this.classList.toggle("active"); // remove and add the class of the selected section 
+        var reveal = this.lastElementChild; // get section last child 
+        if (reveal.style.display === "block") {
+            reveal.style.display = "none";
+            reveal.classList.remove('effect');
+            // reveal.style.animation = 'svg-color 5s linear 1 1s';
+
+        } else {
+            reveal.style.display = "block";
+            reveal.classList.add('effect');
+
+
+        }
+    });
+}
+
+// parentContainer.addEventListener('click', function (e) {
+//     const target = e.target;
+//     console.log(target)
+
+
+//     if (target.matches('#icons')) {
+
+//         if (answerContainer.style.display = 'none') {
+//             e.target.parentNode.parentNode.lastElementChild.style.display = 'block'
+
+//         } else {
+//             e.target.parentNode.parentNode.lastElementChild.style.display = 'none'
+
+//         }
+//     }
+
+// })
